b3264f7641338244800b5230aea62ca518af0767
Abdullah <abdullah@prodoo.com>
2025-10-10 22:03:15 +0500

    dashboard donations fixes

--- static/config.js
+++ static/config.js
@@ -1,3 +1,6 @@
-// config.js 
-// const API_BASE = "http://localhost:5000";
-const API_BASE = "https://api.khcrf.org";
+// config.js
+const API_BASE = "http://localhost:5000";
+// const API_BASE = "https://api.khcrf.org";
+const config = {
+  API_BASE,
+};
