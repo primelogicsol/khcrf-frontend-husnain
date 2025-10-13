161ed318296698893ea344316c691ad8c3d878ae
Abdullah <abdullah@prodoo.com>
2025-10-13 22:32:14 +0500

    backend url fixed

--- static/config.js
+++ static/config.js
@@ -1,6 +1,6 @@
 // config.js
-const API_BASE = "http://localhost:5000";
-// const API_BASE = "https://api.khcrf.org";
+// const API_BASE = "http://localhost:5000";
+const API_BASE = "https://api.khcrf.org";
 const config = {
   API_BASE,
 };
